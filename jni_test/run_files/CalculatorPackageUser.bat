72f4ad336c9c62f674e9e8979a57d8692d3ad313
quasiben <quasiben@c20180e2-7049-495a-b602-474bccf0edbc>
2008-08-20 19:54:27 +0000

    cleaning repository
    git-svn-id: http://code.compucell3d.org/tmp-svn/branch@421 c20180e2-7049-495a-b602-474bccf0edbc

--- jni_test/run_files/CalculatorPackageUser.bat
+++
@@ -1,5 +0,0 @@
-
-@echo off
-set PATH=C:\Program Files\classes\CalculatorTry;%PATH%
-javac CalculatorPackageUser.java
-java CalculatorPackageUser
--- jni_test/run_files/main.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set PATH=C:\Program Files\classes\CalculatorTry;%PATH%
-set PATH=c:\Program Files\Java\jdk1.6.0_06\jre\bin\server\;%PATH%
-set CLSASSPATH=C:\JavaProjects\Shapes\bin;%CLASSPATH%
-main.exe
