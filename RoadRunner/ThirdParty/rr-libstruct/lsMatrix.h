778197c2e3eaa0b4f5aaf5bdd4e38f8129b2bf25
mitja <mitja at indiana>
2013-06-28 14:23:41 -0400

    applied some minor build changes to support Mac OS X: * updated std=c++ dialect on Linux vs Mac OS X - deal with differences with whether or not isnan is defined as a macro * changed __linux to __unix || __APPLE__ - can be treated identically in most cases

--- RoadRunner/ThirdParty/rr-libstruct/lsMatrix.h
+++ RoadRunner/ThirdParty/rr-libstruct/lsMatrix.h
@@ -1,8 +1,9 @@
 #ifndef lsMatrixH
 #define lsMatrixH
 
-#if defined(__linux)
-#include <cstring> //FOr memset on linux
+//     (from Andy S.)
+#if defined __unix || defined(__APPLE__)
+#include <cstring> //for memset on unix platforms
 #endif
 #include <ostream>
 #include "lsExporter.h"
