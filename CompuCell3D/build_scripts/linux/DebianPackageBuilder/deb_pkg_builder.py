3207c0dea01096a4f31732a0544108a87d8d17d9
maciekswat <maciekswat@gmail.com>
2013-09-10 15:16:51 -0400

    small changes to debian builder puthon script

--- CompuCell3D/build_scripts/linux/DebianPackageBuilder/deb_pkg_builder.py
+++ CompuCell3D/build_scripts/linux/DebianPackageBuilder/deb_pkg_builder.py
@@ -31,14 +31,12 @@ def replaceTemplate(tplFileName,replaceDict={}):
 
 def modifyRunScripts(fileName,location_on_target_system):
     targetSystemLocation=os.path.abspath('/'+location_on_target_system)
-    #print 'targetSystemLocation=',targetSystemLocation
     
-    #outFileName,ext=os.path.splitext(tplFileName)    
     tmpFileName=os.path.abspath(fileName+'.tmp')
     
     tmpFile=open(tmpFileName,'w')
     inFile=open(fileName,'r')
-    #outFile=open(outFileName,'w')
+    
     for line in inFile.readlines():
         line=line.rstrip()
         prefix_match=re.match('[\s]*export[\s]*PREFIX_CC3D',line)
@@ -47,20 +45,11 @@ def modifyRunScripts(fileName,location_on_target_system):
             print >>tmpFile,'export PREFIX_CC3D='+targetSystemLocation
         else:
             print>>tmpFile,line    
-        #newLineTmp=str(line)
-        #for tplName,replaceVal in replaceDict.iteritems():
-            #newLineTmp=re.sub(tplName,replaceVal,newLineTmp)
-            #newLineTmp=newLineTmp.rstrip()
-            ##print 'newLineTmp=',newLineTmp
-        
-        #print>>outFile,newLineTmp    
         
     inFile.close()
     tmpFile.close()
     shutil.move(tmpFileName,fileName)
     subprocess.call(['chmod','0755',fileName])
-    #outFile.close()
-    #os.remove(tplFileName)
     
     
 
@@ -205,203 +194,3 @@ print 'DONE'
 
 
 
-sys.exit()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#copy cc3d installation directory to Debian package placeholder directory
-cc3d_debian_pkg_dest_dir = os.path.join(package_path,'usr/share/compucell3d')
-print 'cc3d_debian_pkg_dest_dir=',cc3d_debian_pkg_dest_dir
-print 'cc3d_install_dir=',cc3d_install_dir
-
-# copy compucell3d into the debian package dest dir
-shutil.rmtree(cc3d_debian_pkg_dest_dir)
-shutil.copytree(cc3d_install_dir,cc3d_debian_pkg_dest_dir)
-
-# check the size of the debian package
-# we can use shell=True in subprocess Popen instead
-p1=Popen(['du','-s',package_path],stdout=PIPE)
-p2=Popen(['awk','{ print $1; }' ],stdin=p1.stdout, stdout=PIPE)
-out, err = p2.communicate()
-package_size=out
-print 'size of debian package=',out
-
-#replacing tpl files with target files
-#cc3d_desktop_tpl = os.path.join(package_path,'usr/share/applications/compucell3d.desktop.tpl')
-#twedit_desktop_tpl = os.path.join(package_path,'usr/share/applications/twedit++.desktop.tpl')
-#celldraw_desktop_tpl = os.path.join(package_path,'usr/share/applications/celldraw.desktop.tpl')    
-
-replaceTemplate(os.path.join(package_path,'usr/share/applications/compucell3d.desktop.tpl'),{'<VERSION>':version})
-replaceTemplate(os.path.join(package_path,'usr/share/applications/twedit++.desktop.tpl'),{'<VERSION>':version})
-replaceTemplate(os.path.join(package_path,'usr/share/applications/celldraw.desktop.tpl'),{'<VERSION>':version})
-replaceTemplate(os.path.join(package_path,'DEBIAN/control.tpl'),{'<VERSION>':version,'<INSTALLED_SIZE>':str(package_size)})    
-    
-    #elif line.startswith('!define INSTALLER_NAME'):
-        #line='!define INSTALLER_NAME '+'"'+INSTALLER_NAME+'"'
-    #elif line.startswith('!define INSTALLATION_SOURCE_DIR'):
-        #line = '!define INSTALLATION_SOURCE_DIR '+'"'+INSTALLATION_SOURCE_DIR+'"'
-    #elif  line.startswith('<INSTALL_FILES>') :
-        #line=''
-        #for install_line in INSTALL_FILES:
-            #line+=install_line+'\n'
-            
-    #elif  line.startswith('<DELETE_FILES>') :
-        #line=''
-        #for delete_line in DELETE_FILES:
-            #line+=delete_line+'\n'
-            
-    
-    
-
-    
-
-
-sys.exit()
-
-## this is the path to the NSIS instaler executable
-#NSIS_EXE_PATH='C:\Program Files (x86)\NSIS\makensis.exe '
-
-## version has to have format 3.7.0.0 - four numbers otherwise NSIS crashes, strange...
-
-
-#cc3d_install_dir=options.dirname
-#cc3d_install_dir=os.path.abspath(cc3d_install_dir)
-
-
-#cwd=os.path.abspath(os.getcwd())
-
-##revision number 
-#from datetime import date
-#today=date.today()
-#revisionNumber=str(today.year)+str(today.month).zfill(2)+str(today.day).zfill(2)
-#version=options.version
-
-#INSTALLER_NAME=options.installer_name
-
-#if INSTALLER_NAME=='':
-    #INSTALLER_NAME=os.path.abspath(os.path.join(cwd,'setup-'+version+'v'+revisionNumber+'.exe'))
-#else:    
-    #INSTALLER_NAME=os.path.abspath(INSTALLER_NAME)
-    
-#INSTALLATION_SOURCE_DIR=cc3d_install_dir
-
-
-#INSTALL_FILES=[] # holds lines for install section of the installer
-#DELETE_FILES=[]  # holds lines for delete section of the uninstaller
-
-## ----------creating lines for install section of the installer
-#sub=''
-#for root, subfolders, files in os.walk(cc3d_install_dir):
-    #relpath=os.path.relpath(root,cc3d_install_dir )    
-    #separator='\\'
-    #if len(relpath)==1:
-        #INSTALL_FILES.append('SetOutPath '+'"$INSTDIR"')
-
-    #else:
-        #INSTALL_FILES.append('SetOutPath '+'"$INSTDIR'+separator+relpath+'"')
-    
-    
-    #for file in files:
-        #INSTALL_FILES.append('File '+'"'+os.path.join(root,file)+'"')
-
-## ---------- end creating lines for install section of the installer    
-
-
-## ----------creating lines for install section of the uninstaller
-
-#sub=''
-#dirs_to_remove=[]
-
-#for root, subfolders, files in os.walk(cc3d_install_dir):
-    #relpath=os.path.relpath(root,cc3d_install_dir )    
-    
-    #if len(relpath)!=1:
-        #dirs_to_remove.insert(0,'"$INSTDIR\\'+relpath+'"')
-    
-    
-    #for file in files:
-        #relpath_file=os.path.relpath(os.path.join(root,file),cc3d_instalinstaller_path=os.path.dirname(INSTALLER_NAME)
-
-##inFile=open('CompuCell3D.nsi.tpl','r')
-##outFile=open(os.path.join(installer_path,'CompuCell3D_installer.nsi'),'w')
-##for line in inFile.readlines():
-    ##line=line.rstrip()
-    ##if line.startswith('!define VERSION'):
-        ##line='!define VERSION '+'"'+version+'"'
-    ##elif line.startswith('!define INSTALLER_NAME'):
-        ##line='!define INSTALLER_NAME '+'"'+INSTALLER_NAME+'"'
-    ##elif line.startswith('!define INSTALLATION_SOURCE_DIR'):
-        ##line = '!define INSTALLATION_SOURCE_DIR '+'"'+INSTALLATION_SOURCE_DIR+'"'
-    ##elif  line.startswith('<INSTALL_FILES>') :
-        ##line=''
-        ##for install_line in INSTALL_FILES:
-            ##line+=install_line+'\n'
-            
-    ##elif  line.startswith('<DELETE_FILES>') :
-        ##line=''
-        ##for delete_line in DELETE_FILES:
-            ##line+=delete_line+'\n'
-            
-    ##print>>outFile,line    
-    
-
-    
-##inFile.close()
-##outFile.close()l_dir )   
-        ##DELETE_FILES.append('Delete '+ '"$INSTDIR\\'+relpath_file+'"')        
-
-##DELETE_FILES.append('')
-        
-
-##for dir in dirs_to_remove:
-    ##DELETE_FILES.append('RmDir '+dir)
-
-## ---------- end of creating lines for install section of the uninstaller
-
-
-
-
-
-##installer_path=os.path.dirname(INSTALLER_NAME)
-
-##inFile=open('CompuCell3D.nsi.tpl','r')
-##outFile=open(os.path.join(installer_path,'CompuCell3D_installer.nsi'),'w')
-##for line in inFile.readlines():
-    ##line=line.rstrip()
-    ##if line.startswith('!define VERSION'):
-        ##line='!define VERSION '+'"'+version+'"'
-    ##elif line.startswith('!define INSTALLER_NAME'):
-        ##line='!define INSTALLER_NAME '+'"'+INSTALLER_NAME+'"'
-    ##elif line.startswith('!define INSTALLATION_SOURCE_DIR'):
-        ##line = '!define INSTALLATION_SOURCE_DIR '+'"'+INSTALLATION_SOURCE_DIR+'"'
-    ##elif  line.startswith('<INSTALL_FILES>') :
-        ##line=''
-        ##for install_line in INSTALL_FILES:
-            ##line+=install_line+'\n'
-            
-    ##elif  line.startswith('<DELETE_FILES>') :
-        ##line=''
-        ##for delete_line in DELETE_FILES:
-            ##line+=delete_line+'\n'
-            
-    ##print>>outFile,line    
-    
-
-    
-##inFile.close()
-##outFile.close()
-
-###executing NSIS command
-##import subprocess
-##subprocess.call([NSIS_EXE_PATH,'/V1','CompuCell3D_installer.nsi'])
