26644982b275f305ce1698ff539558f454f52f18
mswat <mswat@indiana.edu>
2013-11-11 16:47:37 -0500

    fixed minor issues with twedit++ - EOL and icon on OSX. restored PIFInitializer.cpp

--- CompuCell3D/Twedit++/EditorWindow.py
+++ CompuCell3D/Twedit++/EditorWindow.py
@@ -1160,6 +1160,14 @@ class EditorWindow(QMainWindow):
         _editor.setCaretLineBackgroundColor(QtGui.QColor('#EFEFFB'))        
         # _editor.modificationChanged.connect(self.modificationChangedSlot)
         
+        
+        if not sys.platform.startswith('win'):        
+            _editor.setEolMode(QsciScintilla.EolWindows) # SETTING EOL TO WINDOWS MESSES THINGS UP AS SAVE FCN (MOST LIKELY)ADS EXTRA CR SIGNS -
+        else:
+            _editor.setEolMode(QsciScintilla.EolUnix) # SETTING EOL TO WINDOWS MESSES THINGS UP AS SAVE FCN (MOST LIKELY)ADS EXTRA CR SIGNS - 
+            
+        # _editor.setEolMode(QsciScintilla.EolMac) # SETTING EOL TO WINDOWS MESSES THINGS UP AS SAVE FCN (MOST LIKELY)ADS EXTRA CR SIGNS - 
+        # _editor.setUtf8(True) # using UTF-8 encoding        
         # _editor.setEolMode(QsciScintilla.EolWindows) # SETTING EOL TO WINDOWS MESSES THINGS UP AS SAVE FCN (MOST LIKELY)ADS EXTRA CR SIGNS - 
         # _editor.setEolMode(QsciScintilla.EolUnix) # SETTING EOL TO WINDOWS MESSES THINGS UP AS SAVE FCN (MOST LIKELY)ADS EXTRA CR SIGNS - 
         # _editor.setEolMode(QsciScintilla.EolMac) # SETTING EOL TO WINDOWS MESSES THINGS UP AS SAVE FCN (MOST LIKELY)ADS EXTRA CR SIGNS - 
