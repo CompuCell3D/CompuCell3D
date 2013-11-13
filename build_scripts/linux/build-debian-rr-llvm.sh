680257c29b37102df8c0f64772ff794e440ba140
mswat <mswat@indiana.edu>
2013-11-13 10:59:28 -0500

    building new compilation scripts - this commits has only initial drafts of them

---
+++ build_scripts/linux/build-debian-rr-llvm.sh
@@ -0,0 +1,162 @@
+# example command ./build-debian-rr-llvm.sh -s=~/RR_LLVM -p=~/install_projects/RR_LLVM -c=4
+#command line parsing
+
+function run_and_watch_status {
+    # first argument is a task descriptor and is mandatory
+    # the remaining arguments make up a command to execute
+    
+    #executing the command
+    "${@:2}"
+    # querrying its status
+    status=$?
+    echo "STATUS=$status"
+    if [ $status -ne 0 ]; then
+        echo "error with $1"
+        exit
+    fi
+    return $status    
+
+}
+
+
+export BUILD_ROOT=~/install_projects/RR_LLVM_build
+export SOURCE_ROOT=~/RR_LLVM
+export DEPENDENCIES_ROOT=~/install_projects
+export INSTALL_PREFIX=~/install_projects/RR_LLVM
+
+
+export BUILD_RR=NO
+export BUILD_RR_DEPEND=NO
+export BUILD_ALL=YES
+export MAKE_MULTICORE=1
+
+
+for i in "$@"
+do
+case $i in
+    -p=*|--prefix=*)
+    INSTALL_PREFIX=`echo $i | sed 's/[-a-zA-Z0-9]*=//'`
+
+    ;;
+    -s=*|--source-root=*)
+    SOURCE_ROOT=`echo $i | sed 's/[-a-zA-Z0-9]*=//'`
+    ;;
+    
+    -c=*|--cores=*)
+    MAKE_MULTICORE=`echo $i | sed 's/[-a-zA-Z0-9]*=//'`
+    ;;
+    --cc3d)
+    BUILD_ALL=NO
+    BUILD_CC3D=YES
+    ;;
+    --rr)
+    BUILD_ALL=NO
+    BUILD_RR=YES
+    ;;
+    --rr-depend)
+    BUILD_ALL=NO
+    BUILD_RR_DEPEND=YES
+    ;;
+    --help)
+    
+    echo "build-debian-rr-llvm.sh <OPTIONS>"
+    echo
+    echo "OPTIONS:"
+    echo
+    echo "-p=<dir> | --prefix=<dir> : cc3d installation prefix (target directory) | default ~/install_projects/cc3d"
+    echo
+    echo "-b=<dir> | --build_root=<dir> : temporary directory for object files (aka compiler output) | default ~/install_projects/RR_LLVM_build"
+    echo
+    echo "-s=<dir> | --source_root=<dir> : root directory of RR_LLVM repository "
+    echo
+    echo "specifying options below will allow for selection of specific projects to build."
+    echo  "If you are rebuilding CompuCell3D by picking specific projects you will shorten build time"
+    echo
+    echo "--rr : builds RoadRunner"
+    echo
+    echo "--rr-depend : builds RoadRunner dependencies"
+    echo
+    ;;
+    
+    *)
+            # unknown option
+    ;;
+esac
+done
+
+
+
+if [ "$BUILD_ALL" == YES ]
+then
+  BUILD_RR_DEPEND=YES
+  BUILD_ALL=YES
+fi
+
+echo BUILD_RR $BUILD_RR_DEPEND
+echo BUILD_RR_DEPEND $BUILD_RR_DEPEND
+
+
+# expanding paths
+eval INSTALL_PREFIX=$INSTALL_PREFIX
+eval BUILD_ROOT=$BUILD_ROOT
+eval SOURCE_ROOT=$SOURCE_ROOT
+eval DEPENDENCIES_ROOT=$DEPENDENCIES_ROOT
+
+
+BUILD_ROOT=${INSTALL_PREFIX}_build
+DEPENDENCIES_ROOT=${INSTALL_PREFIX}_depend
+
+echo INSTALL_PREFIX = ${INSTALL_PREFIX}
+echo BUILD_ROOT = ${BUILD_ROOT}
+echo SOURCE_ROOT = ${SOURCE_ROOT}
+echo DEPENDENCIES_ROOT = ${DEPENDENCIES_ROOT}
+
+
+
+echo MAKE_MULTICORE= $MAKE_MULTICORE
+MAKE_MULTICORE_OPTION=-j$MAKE_MULTICORE
+echo OPTION=$MAKE_MULTICORE_OPTION
+
+
+mkdir -p $BUILD_ROOT
+mkdir -p $DEPENDENCIES_ROOT
+
+
+if [ "$BUILD_RR_DEPEND" == YES ]
+then
+  ############# BUILDING CC3D
+  mkdir -p $BUILD_ROOT/third_party
+  cd $BUILD_ROOT/third_party
+  
+
+  #checking if kernel is 32 or 64 bit
+  # BITS=$( getconf LONG_BIT )
+
+  # echo BITS = $BITS
+
+  # OPTIMIZATION_FLAGS= 
+
+  # if [ ! $BITS -eq 64 ]
+  # then
+      
+      # # since you cannot pass cmake options that have spaces to cmake command line the alternatice is to prepare initial CmakeCache.txt file and put those options there...
+      # echo 'CMAKE_C_FLAGS_RELEASE:STRING=-O0 -DNDEBUG'>>CMakeCache.txt
+  # fi  
+  
+
+  run_and_watch_status THIRD_PARTY_CMAKE_CONFIG cmake -G "Unix Makefiles"  -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_PREFIX} -DCMAKE_BUILD_TYPE:STRING=Release $SOURCE_ROOT/third_party
+  run_and_watch_status THIRD_PARTY_COMPILE_AND_INSTALL make $MAKE_MULTICORE_OPTION VERBOSE=1 && make install
+  ############# END OF BUILDING CC3D
+fi
+
+if [ "$BUILD_RR" == YES ]
+then
+  ############# BUILDING CC3D
+  mkdir -p $BUILD_ROOT/RR_LLVM
+  cd $BUILD_ROOT/RR_LLVM
+
+
+  run_and_watch_status RR_CMAKE_CONFIG cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_PREFIX}  -DTHIRD_PARTY_INSTALL_FOLDER:PATH=${INSTALL_PREFIX}  -DCMAKE_BUILD_TYPE:STRING=Release $SOURCE_ROOT 
+  run_and_watch_status RR_COMPILE_AND_INSTALL make $MAKE_MULTICORE_OPTION VERBOSE=1 && make install
+  ############# END OF BUILDING CC3D
+fi
