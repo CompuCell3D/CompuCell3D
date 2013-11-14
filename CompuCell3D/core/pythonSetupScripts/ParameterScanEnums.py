9890d11ac72d7656091260fc2074cd30565d1951
maciekswat <maciekswat@gmail.com>
2013-11-14 14:01:26 -0500

    Dictionary comprehensions not supported on <2.6. Had to revert to manual code...

--- CompuCell3D/core/pythonSetupScripts/ParameterScanEnums.py
+++ CompuCell3D/core/pythonSetupScripts/ParameterScanEnums.py
@@ -4,9 +4,17 @@
 #reverse lookup
 TYPE_DICT={XML_ATTR:'XML_ATTR',XML_CDATA:'XML_CDATA',PYTHON_GLOBAL:'PYTHON_GLOBAL'}
 
-TYPE_DICT_REVERSE={v:k for k, v in TYPE_DICT.items()}
+TYPE_DICT_REVERSE={} # dictionry comprehension does not work for 2.6 and earlier... thank you Apple
+for k, v in TYPE_DICT.items():
+	TYPE_DICT_REVERSE[v]=k
+
+
 
 (FLOAT,INT,STRING)=range(3)
 #reverse lookup
 VALUE_TYPE_DICT={FLOAT:'float',INT:'int',STRING:'string'}
-VALUE_TYPE_DICT_REVERSE={v:k for k, v in VALUE_TYPE_DICT.items()}
+VALUE_TYPE_DICT_REVERSE={} # dictionry comprehension does not work for 2.6 and earlier... thank you Apple
+for k, v in VALUE_TYPE_DICT.items():
+	VALUE_TYPE_DICT_REVERSE[v]=k
+
+
