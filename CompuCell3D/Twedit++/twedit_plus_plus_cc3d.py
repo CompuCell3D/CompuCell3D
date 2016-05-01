08459480ce400a304635db63d6c228db42156ffe
m <m@ms-Mac.local>
2016-05-01 16:10:58 -0700

    added message event handlers to twedit and player to avid spurious warnings from qt on el-capitan

--- CompuCell3D/Twedit++/twedit_plus_plus_cc3d.py
+++ CompuCell3D/Twedit++/twedit_plus_plus_cc3d.py
@@ -12,7 +12,7 @@ def setPaths():
             sys.path.insert(0,environ["PYTHON_DEPS_PATH"])
             sys.path.append(environ["SWIG_LIB_INSTALL_DIR"])
             sys.path.append(environ["PYTHON_MODULE_PATH"])
-            
+
         except:
             pass
     else:
@@ -20,8 +20,8 @@ def setPaths():
             sys.path.append(environ["SWIG_LIB_INSTALL_DIR"])
             sys.path.append(environ["PYTHON_MODULE_PATH"])
         except:
-            pass    
-          
+            pass
+
 setPaths()
 
 
@@ -35,6 +35,22 @@ from PyQt4.Qsci import *
 
 
 import sys
+import PyQt4
+#instaling message handler to suppres spurious qt messages
+if sys.platform=='darwin':
+    import platform
+    mac_ver = platform.mac_ver()
+    mac_ver_float = float('.'.join(mac_ver[0].split('.')[:2]))
+    if mac_ver_float == 10.11:
+
+        def handler(msg_type, msg_log_context, msg_string=None):
+            if msg_log_context.startswith('QCocoaView handleTabletEvent'):
+                return
+            print msg_log_context
+
+        PyQt4.QtCore.qInstallMsgHandler(handler)
+
+
 
 
 
@@ -54,7 +70,7 @@ setDebugging(0)
 
 
 
-        
+
 
 
 class Twedit(object):
@@ -73,30 +89,30 @@ class Twedit(object):
                 # if e.errno == 13:
                     # dbgMsg("Another instance is already running, quitting.")
                     # raise
-                    
+
                 # dbgMsg(e.errno)
                 # raise
         # else: # non Windows
             # import fcntl, sys
-            
+
             # try:
                 # self.fp = open(self.lockfile, 'w')
-                # fcntl.lockf(self.fp, fcntl.LOCK_EX | fcntl.LOCK_NB)                
+                # fcntl.lockf(self.fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
             # except IOError:
-                # dbgMsg("NON-WINDOWS PLATFORM Another instance is already running, quitting.")                
+                # dbgMsg("NON-WINDOWS PLATFORM Another instance is already running, quitting.")
                 # raise OSError
-    
+
     # def __del__(self):
-    
+
         # import sys
         # if sys.platform == 'win32':
             # if hasattr(self, 'fd'):
                 # os.close(self.fd)
                 # os.unlink(self.lockfile)
-                
+
     def getFileList(self):
         return self.fileList
-        
+
     def processCommandLineOptions(self):
         import getopt
 
@@ -113,7 +129,7 @@ class Twedit(object):
             # self.usage()
             sys.exit(2)
         port=47406
-        
+
         for o, a in opts:
             print "o=",o
             print "a=",a
@@ -123,41 +139,41 @@ class Twedit(object):
             if o in ("--file"):
                 file=a
                 print "THIS IS file=",file
-                
-        for  a in args:                
+
+        for  a in args:
             self.fileList.append(a)
-            # elif o in ("--exitWhenDone"):             
-                # self.closePlayerAfterSimulationDone=True 
-                
+            # elif o in ("--exitWhenDone"):
+                # self.closePlayerAfterSimulationDone=True
+
             # else:
                 # print "GOT ARGUMENT:", a
                 # assert False, "unhandled option"
-        print "FILE LIST=",self.fileList        
+        print "FILE LIST=",self.fileList
 
-    
+
     def main(self,argv):
-        
-        
+
+
         #global mainWindow
         app = CQApplication(argv)
 
-        qtVersion=str(QT_VERSION_STR).split('.') 
+        qtVersion=str(QT_VERSION_STR).split('.')
         import platform
-        
+
         if platform.mac_ver()[0]!='' and qtVersion[1]>=2: # style sheets may not work properly for qt < 4.2
             app.setStyleSheet( "QDockWidget::close-button, QDockWidget::float-button { padding: 0px;icon-size: 24px;}")
-        
 
 
 
 
 
-        
+
+
         pixmap = QPixmap("icons/lizard-at-a-computer-small.png")
         print "pixmap=",pixmap
         splash = QSplashScreen(pixmap)
         splash.showMessage("Please wait.\nLoading Twedit++ ...",Qt.AlignLeft,  Qt.black)
-        splash.show()        
+        splash.show()
 
         app.processEvents()
         #app.connect(app, SIGNAL("lastWindowClosed()"), app, SLOT("quit()"))
@@ -166,49 +182,47 @@ class Twedit(object):
 
         self.mainWindow.show()
         splash.finish(self.mainWindow)
-        
+
         # self.mainWindow.processCommandLine()
         self.mainWindow.openFileList(self.fileList)
         QApplication.setWindowIcon(QIcon("Twedit++/icons/twedit-icon.png"))
 
         self.mainWindow.raise_() # to make sure on OSX window is in the foreground
-        
-        if sys.platform.startswith('win'):    
+
+        if sys.platform.startswith('win'):
             import win32process
             self.mainWindow.setProcessId(win32process.GetCurrentProcessId())
             # showTweditWindowInForeground()
-        
+
         app.exec_()
 
-        
+
 
 if __name__ == '__main__':
 
     try:
         twedit=Twedit()
-        twedit.processCommandLineOptions()    
+        twedit.processCommandLineOptions()
     except OSError,e:
         dbgMsg("GOT OS ERROR")
-           
+
         # argvSendSocket=QUdpSocket()
         fileList=twedit.getFileList()
         print "\n\n\n\n FILE LIST=",fileList
-        for fileName in fileList: 
+        for fileName in fileList:
             datagram=fileName
-            # argvSendSocket.writeDatagram(datagram,QHostAddress.LocalHost,47405)        
-            fileSender=FileNameSender(datagram)  
+            # argvSendSocket.writeDatagram(datagram,QHostAddress.LocalHost,47405)
+            fileSender=FileNameSender(datagram)
             fileSender.send()
-            
-        if sys.platform == 'win32':    
+
+        if sys.platform == 'win32':
             showTweditWindowInForeground()
         else:
-            # notice, on linux you may have to change "focus stealing prevention level" setting to None in window behavior settings , to enable bringing window to foreground 
+            # notice, on linux you may have to change "focus stealing prevention level" setting to None in window behavior settings , to enable bringing window to foreground
             dbgMsg("NON-WINDOWS PLATFORM - TRY TO ACTIVATE WINDOW")
-        
-            
-        # sys.exit()    
 
-    
+
+        # sys.exit()
+
+
     twedit.main(sys.argv[1:])
-    
-    
