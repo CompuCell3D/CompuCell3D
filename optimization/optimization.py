082d44f215117d8b392f7aedff8996f3858f4600
Maciek Swat <maciekswat@gmail.com>
2017-03-22 20:32:25 -0400

    added code to automatically detemine push and pull ports , allowing multiple simulaiton runs int he same machine

--- optimization/optimization.py
+++ optimization/optimization.py
@@ -138,9 +138,12 @@ class Optimizer(object):
     def __init__(self):
 
         self.param_set_list = [1, 2, 3, 4, 5]
-        self.context = zmq.Context()
-        self.push_address_str = "tcp://127.0.0.1:5557"
-        self.pull_address_str = "tcp://127.0.0.1:5558"
+        self.push_context = zmq.Context()
+        self.push_port = None
+        self.pull_port = None
+        self.core_ip_address = "tcp://127.0.0.1"
+        # self.push_address_str = "tcp://127.0.0.1:5557"
+        # self.pull_address_str = "tcp://127.0.0.1:5558"
 
         #
         # dictionary that specifies all the information that worker neeeds to run simulation
@@ -155,8 +158,24 @@ class Optimizer(object):
         # parsed command line arguments
         self.parse_args = None
 
-        self.zmq_socket = self.context.socket(zmq.PUSH)
-        self.zmq_socket.bind(self.push_address_str)
+
+        self.push_socket = self.push_context.socket(zmq.PUSH)
+        self.push_port = self.push_socket.bind_to_random_port(self.core_ip_address, min_port=5557, max_port=6557, max_tries=100)
+        self.push_address_str = self.core_ip_address+":%s"%str(self.push_port)
+
+        self.pull_context = zmq.Context()
+        self.pull_socket = self.pull_context.socket(zmq.PULL)
+        self.pull_port = self.pull_socket.bind_to_random_port(self.core_ip_address, min_port=5557, max_port=6557, max_tries=100)
+        self.pull_address_str = self.core_ip_address + ":%s" % str(self.pull_port)
+        # self.results_receiver.bind(self.pull_address_str)
+
+
+        # self.zmq_socket.bind(self.push_address_str)
+
+
+
+        # self.zmq_socket = self.context.socket(zmq.PUSH)
+        # self.zmq_socket.bind(self.push_address_str)
         self.num_workers = 1
 
     def acknowledge_presence(self, num_workers):
@@ -181,9 +200,11 @@ class Optimizer(object):
         :return: None
         """
 
-        context = zmq.Context()
-        results_receiver = context.socket(zmq.PULL)
-        results_receiver.bind(self.pull_address_str)
+        # context = zmq.Context()
+        # results_receiver = context.socket(zmq.PULL)
+        # results_receiver.bind(self.pull_address_str)
+        results_receiver = self.pull_socket
+
         return_data_dict = {}  # {worker_tag:return_value}
 
         sum = 0
@@ -282,7 +303,7 @@ class Optimizer(object):
             param_set_dict[worker_tag] = param
 
             # self.zmq_socket.send_json(param_dict)
-            self.zmq_socket.send_json(workload_dict)
+            self.push_socket.send_json(workload_dict)
 
             print 'sent = ', workload_dict
 
