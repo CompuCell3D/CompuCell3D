a5139b61781c6b636773ebf378aa532b23d5971e
compucell3d <compucell3dproject@gmail.com>
2017-11-11 13:24:05 -0500

    added docstring

--- CompuCell3D/player5/Configuration/DefaultSettingData.py
+++ CompuCell3D/player5/Configuration/DefaultSettingData.py
@@ -1,1 +1,5 @@
-from DefaultSettingData import * 
+"""
+The purpose of this module is to "overshadow" any leftover player5/Configuration/DefaultDataSettings.pyc
+This is especially needed on Windows
+"""
+from DefaultSettingData import *
