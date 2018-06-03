04694c60bbf9df730e3dac4bafcc4234a4a5275f
Maciek Swat <maciekswat@gmail.com>
2018-06-02 23:36:30 -0400

    formatting

--- CompuCell3D/player5/steering/SteeringPanelView.py
+++ CompuCell3D/player5/steering/SteeringPanelView.py
@@ -1,12 +1,15 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import *
 
-class SteeringPanelView(QtWidgets.QTableView):
 
+class SteeringPanelView(QtWidgets.QTableView):
     def __init__(self, *args, **kwargs):
+
         QtWidgets.QTableView.__init__(self, *args, **kwargs)
-        # self.yourView.setEditTriggers(QtWidgets.AllEditTriggers)
+
+
     def mousePressEvent(self, event):
+
         if event.button() == Qt.LeftButton:
             index = self.indexAt(event.pos())
             col_name = self.get_col_name(index)
@@ -14,7 +17,7 @@ class SteeringPanelView(QtWidgets.QTableView):
                 self.edit(index)
         else:
             super(SteeringPanelView, self).mousePressEvent(event)
-        # QTableView.mousePressEvent(event)
+            # QTableView.mousePressEvent(event)
 
     def get_col_name(self, index):
 
@@ -30,8 +33,8 @@ class SteeringPanelView(QtWidgets.QTableView):
         Implements basic size hint
         :return: {Qsize}
         """
-        row_count = min(self.model().rowCount()+1, 20)
+        row_count = min(self.model().rowCount() + 1, 20)
         row_size = self.rowHeight(0)
 
-        sugested_vsize = row_count*row_size
-        return QSize(600,sugested_vsize)
+        sugested_vsize = row_count * row_size
+        return QSize(600, sugested_vsize)
