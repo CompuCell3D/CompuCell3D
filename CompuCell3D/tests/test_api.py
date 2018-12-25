9a23e566f561172f8ec9ad9b293e08a75809ea9c
Maciek Swat <maciekswat@gmail.com>
2018-12-25 16:12:39 -0700

    working on test suite

---
+++ CompuCell3D/tests/test_api.py
@@ -0,0 +1,76 @@
+import unittest
+
+
+
+class TestAPI(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_core_functionality(self):
+        try:
+            import CompuCell
+        except:
+            self.fail("Could not import CompuCell module")
+
+        # sim = CompuCell.Simulator()
+        pt_1 = CompuCell.Point3D()
+        pt_2 = CompuCell.Point3D(1, 1, 1)
+        print('pt=', pt_1)
+
+        potts = CompuCell.Potts3D()
+        print('potts=', potts)
+        print('potts.getTemperature()=', potts.getTemperature())
+
+        baby_sim = CompuCell.BabySim()
+
+        print('potts=', baby_sim.getPotts())
+
+
+        # print('_b_strategy=', bs.getBoundaryStrategy())
+
+        boundary_strategy = baby_sim.getBoundaryStrategy()
+        # bbs = baby_sim.getBabyBoundaryStrategy()
+        bbs = baby_sim.getBoundaryStrategy()
+
+
+
+        print('_baby_strategy=', bbs)
+
+
+        # s = CompuCell.Simulator()
+        print('ps=',baby_sim.ps)
+
+        print('bppd=',baby_sim.bppd)
+
+        baby_sim.bppd.Boundary_x("demo")
+
+        sim = CompuCell.Simulator()
+
+        print
+
+        # boundary_strategy = baby_sim.getBoundaryStrategy()
+    def test_core_object_creation_api(self):
+        import CompuCell
+        from cc3d.CompuCellSetup import initModules, parseXML
+
+        xml_fname = r'd:\CC3D_PY3_GIT\CompuCell3D\tests\test_data\cellsort_2D.xml'
+        cc3dXML2ObjConverter = parseXML(xml_fname=xml_fname)
+
+        sim = CompuCell.Simulator()
+
+
+
+        print('num_steps=', sim.ppdCC3DPtr)
+        initModules(sim, cc3dXML2ObjConverter)
+
+        sim.initializeCC3D()
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
