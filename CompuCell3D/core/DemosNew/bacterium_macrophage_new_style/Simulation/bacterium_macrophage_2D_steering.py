da01ff5ae1fffc6fbe998a059b20680371be47ec
Maciek Swat <maciekswat@gmail.com>
2019-02-28 22:56:30 -0700

    added new-stype bacterium macrophage with plots

---
+++ CompuCell3D/core/DemosNew/bacterium_macrophage_new_style/Simulation/bacterium_macrophage_2D_steering.py
@@ -0,0 +1,8 @@
+import cc3d.CompuCellSetup as CompuCellSetup
+
+from bacterium_macrophage_2D_steering_steppables import InventoryCheckSteppable
+
+CompuCellSetup.register_steppable(steppable=InventoryCheckSteppable(frequency=1))
+
+CompuCellSetup.run()
+
