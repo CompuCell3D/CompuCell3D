834eafeab4056d383dca2f4ef559b7f6224d34f5
Maciek Swat <maciekswat@gmail.com>
2019-03-15 23:00:06 -0600

    added docstrings

--- cc3d/core/parameter_scan_utils.py
+++ cc3d/core/parameter_scan_utils.py
@@ -18,7 +18,17 @@ def param_scan_complete_signal(output_dir: Union[str, Path]) -> Path:
     """
     return Path(output_dir).joinpath('param_scan_status.complete')
 
-def handle_param_scan_complete(output_dir:Union[str, Path])->None:
+
+def param_scan_status(output_dir: Union[str, Path]) -> Path:
+    """
+    returns file name for the parameter scan status
+    :param output_dir: {Path, str} main output directory for the parameter scan run
+    :return: {Path}
+    """
+    return Path(output_dir).joinpath('param_scan_status.json')
+
+
+def handle_param_scan_complete(output_dir: Union[str, Path]) -> None:
     """
     Handles parameter scan complete situation
     :param output_dir: {str, Path}
@@ -34,13 +44,18 @@ def handle_param_scan_complete(output_dir:Union[str, Path])->None:
 
     sys.exit(SCAN_FINISHED_OR_DIRECTORY_ISSUE)
 
-def cc3d_proj_pth_in_output_dir(cc3d_proj_fname: str, output_dir: str) -> Path:
+
+def cc3d_proj_pth_in_output_dir(cc3d_proj_fname: Union[str, Path], output_dir: Union[str, Path]) -> Path:
+    """
+    returns path to the .cc3d project in the output directory after the project gets copied
+    to the target folder.
+    todo - this function might me made independent of parameter scan
+
+    :param cc3d_proj_fname:{Path, str} original path to the .cc3d project
+    :param output_dir:{str, Path} path to the outut dir
+    :return:{Path} path to the .cc3d file in the output folder (after the entire project gets copied over)
     """
 
-    :param cc3d_proj_fname:
-    :param output_dir:
-    :return:
-    """
     cc3d_proj_pth = Path(cc3d_proj_fname)
 
     cc3d_proj_dirname = cc3d_proj_pth.parent
@@ -52,9 +67,10 @@ def cc3d_proj_pth_in_output_dir(cc3d_proj_fname: str, output_dir: str) -> Path:
     return cc3d_proj_pth_in_out_dir
 
 
-def copy_project_to_output_folder(cc3d_proj_fname: str, output_dir: str) -> bool:
+def copy_project_to_output_folder(cc3d_proj_fname: Union[str, Path], output_dir: Union[str, Path]) -> bool:
     """
-    Copies entire cc3d project to the output dir. Returns flag whether the operation suceeded or not
+    Copies entire cc3d project to the output dir. Returns flag whether the operation succeeded or not
+    todo - this function might me made independent of parameter scan
     :param cc3d_proj_fname:
     :param output_dir:
     :return:
@@ -70,9 +86,9 @@ def copy_project_to_output_folder(cc3d_proj_fname: str, output_dir: str) -> bool
     return True
 
 
-def parse_param_scan(param_scan_fname: str):
+def parse_param_scan(param_scan_fname: Union[str, Path]) -> None:
     """
-
+    Parses json file that includes parameter scan specification
     :param param_scan_fname:
     :return:
     """
@@ -83,19 +99,21 @@ def parse_param_scan(param_scan_fname: str):
 
 def param_scan_status_path(output_dir: Union[str, Path]) -> Path:
     """
-    given output dir returns path to param scan status file
+    Given output dir returns path to param scan status file
     :param output_dir: {Path}
-    :return:
+    :return:{Path} path to the parameter scan status json file
     """
     return Path(output_dir).joinpath('param_scan_status.json')
 
 
-def create_param_scan_status(cc3d_proj_fname: Union[str,Path], output_dir: str) -> None:
+def create_param_scan_status(cc3d_proj_fname: Union[str, Path], output_dir: Union[str, Path]) -> None:
     """
     Creates parameter scan in the otput directory
-    :param cc3d_proj_fname:{str} file name ofcc3d project that includes parameter scan
+    :param cc3d_proj_fname:{str} file name of .cc3d project that includes parameter scan
+    :param output_dir: {str, Path output folder}
     :return:None
     """
+
     cc3d_simulation_data_handler = CC3DSimulationDataHandler()
     cc3d_simulation_data_handler.readCC3DFileFormat(cc3d_proj_fname)
     cc3d_sim_data = cc3d_simulation_data_handler.cc3dSimulationData
@@ -122,11 +140,26 @@ def create_param_scan_status(cc3d_proj_fname: Union[str,Path], output_dir: str) 
         json.dump(param_scan_root_elem, fout, indent=4)
 
 
-def fetch_next_set_of_scan_parameters(output_dir):
-    # todo add locking
+def fetch_next_set_of_scan_parameters(output_dir: Union[str, Path]) -> dict:
+    """
+    Analyzes param_scan_status.json and picks next set of parameters for parameter scan.
+    Writes updated param_scan_status.json to the disk. the new copy of param_scan_status.json
+    has updated indices of parameters to indicate which iteration has been just complete
+    We assume that we are doing exhaustive parameter scan
+
+    This function performs all operations in an exclusive locking mode which means
+    that users can run multiple instances of parameter scan script and the locking
+    implemented in this function will provide necessary synchronization
+
+    todo - implement other alternative scan methods
+
+    :param output_dir: {str, Path}
+    :return: {dict} a dictionary of {param_label:param_value} type that contains current set of scanned parameters
+    Templating engine will use those values to create actual simulation out of provided template
+    """
+
     with FileLock(Path(output_dir).joinpath('param_scan_status.lock')):
-        # time.sleep(3.0)
-        param_scan_status_path = Path(output_dir).joinpath('param_scan_status.json')
+        param_scan_status_path = param_scan_status(output_dir=output_dir)
 
         with open(param_scan_status_path, 'r') as fin:
             param_scan_status_root = json.load(fin)
@@ -143,8 +176,10 @@ def fetch_next_set_of_scan_parameters(output_dir):
         return ret_dict
 
 
-def update_param_scan_status(param_scan_status_root: dict, output_dir: str) -> dict:
+def update_param_scan_status(param_scan_status_root: dict, output_dir: str) -> None:
     """
+    given root of json element describing parameter scan (param_scan_status_root) and
+    output directory  this function writes updated parameter scan status file to the disk
 
     :param param_list_dict:
     :return:
@@ -153,7 +188,7 @@ def update_param_scan_status(param_scan_status_root: dict, output_dir: str) -> d
 
     param_list_dict = advance_param_list(param_list_dict=param_list_dict)
 
-    param_scan_status_path = Path(output_dir).joinpath('param_scan_status.json')
+    param_scan_status_path = param_scan_status(output_dir=output_dir)
     with open(param_scan_status_path, 'w') as fout:
         json.dump(param_scan_status_root, fout, indent=4)
 
@@ -182,7 +217,8 @@ def next_cartesian_product_from_state(curr_list: List[int], max_list: List[int])
     a generator that gives next cartesian product combination from a current state
     :param curr_list: {list of ints} current cartesian product combination
     :param max_list:{list of ints} maximum values a given position may assume
-    :return:
+    :return: {list} list of indices - one index per parameter that indicate which values should be used for
+    the current combination of scanned parameters
     """
 
     if len(curr_list) != len(max_list):
@@ -207,9 +243,14 @@ def next_cartesian_product_from_state(curr_list: List[int], max_list: List[int])
 
 def run_single_param_scan_simulation(current_scan_parameters: dict, output_dir: str = None):
     """
+    given the set of scanned parameters This funciton creates CC3D project (by applying)
+    parameter set to the .cc3d template and the runs such newly created simulation
+    Runs single CC3D simulation with a
 
-    :param output_dir:
+    :param current_scan_parameters: {str, path} dictionary with current set of parameters for the simulation
+    :param output_dir:{str, Path} output directory
     :return:
     """
+
     print('Running simulation with current_scan_parameters=', current_scan_parameters)
     time.sleep(0.1)
