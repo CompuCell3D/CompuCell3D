d21bcc12aacef5ed803b45e353fdd1f526d40aaa
Maciek Swat <maciekswat@gmail.com>
2019-03-20 22:39:52 -0600

    changed installation layout - now cc3d is a python package

--- cc3d/__init__.py
+++ cc3d/__init__.py
@@ -1,8 +1,8 @@
 import os
 import sys
-from os.path import dirname, join,abspath
+from os.path import dirname, join, abspath
 
-path_postfix=''
+path_postfix = ''
 if sys.platform.startswith('win'):
     path_postfix = '\\'
 
@@ -16,13 +16,12 @@ path_env = os.environ['PATH']
 
 path_env_list = path_env.split(';')
 
-path_env_list = list(map(lambda pth:abspath(pth), path_env_list))
+path_env_list = list(map(lambda pth: abspath(pth), path_env_list))
 
-cc3d_bin_path = abspath(join(cc3d_py_dir,'cpp','bin'))
+cc3d_bin_path = abspath(join(cc3d_py_dir, 'cpp', 'bin'))
 if cc3d_bin_path not in path_env_list:
-    path_env_list.insert(0,cc3d_bin_path)
+    path_env_list.insert(0, cc3d_bin_path)
 
 os.environ['PATH'] = ';'.join(path_env_list)
-
 
 print('ENVIRONMENT VARS=', os.environ)
