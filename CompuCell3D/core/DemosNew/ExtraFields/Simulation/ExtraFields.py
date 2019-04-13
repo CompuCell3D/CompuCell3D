96c03f633401e3538002c5ea832eae361a35df75
Maciek Swat <maciekswat@gmail.com>
2019-04-13 13:24:42 -0600

    fixed one demo and fixed default _settings.sqllite for widnows

--- CompuCell3D/core/DemosNew/ExtraFields/Simulation/ExtraFields.py
+++ CompuCell3D/core/DemosNew/ExtraFields/Simulation/ExtraFields.py
@@ -5,7 +5,7 @@ from .ExtraFieldsSteppables import VectorFieldVisualizationSteppable
 from .ExtraFieldsSteppables import VectorFieldCellLevelVisualizationSteppable
 from .ExtraFieldsSteppables import DiffusionFieldSteppable
 
-CompuCellSetup.register_steppable(steppable=ExtraFieldVisualizationSteppable(frequency=1))
+# CompuCellSetup.register_steppable(steppable=ExtraFieldVisualizationSteppable(frequency=1))
 CompuCellSetup.register_steppable(steppable=IdFieldVisualizationSteppable(frequency=1))
 CompuCellSetup.register_steppable(steppable=VectorFieldVisualizationSteppable(frequency=1))
 CompuCellSetup.register_steppable(steppable=VectorFieldCellLevelVisualizationSteppable(frequency=1))
