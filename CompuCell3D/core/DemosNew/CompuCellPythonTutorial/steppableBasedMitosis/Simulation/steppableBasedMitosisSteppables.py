dfd12d58e7ef25681d89cbdd659624cb05a8ca0f
Maciek Swat <maciekswat@gmail.com>
2019-05-11 20:24:48 -0600

    added steppableBasedMitosis demo

---
+++ CompuCell3D/core/DemosNew/CompuCellPythonTutorial/steppableBasedMitosis/Simulation/steppableBasedMitosisSteppables.py
@@ -0,0 +1,52 @@
+from cc3d.core.PySteppables import *
+
+
+class VolumeParamSteppable(SteppableBasePy):
+    def __init__(self, frequency=1):
+        SteppableBasePy.__init__(self, frequency)
+
+    def start(self):
+        for cell in self.cell_list:
+            cell.targetVolume = 25
+            cell.lambdaVolume = 2.0
+
+    def step(self, mcs):
+        for cell in self.cellList:
+            cell.targetVolume += 1
+
+
+class MitosisSteppable(MitosisSteppableBase):
+    def __init__(self, frequency=1):
+        MitosisSteppableBase.__init__(self, frequency)
+
+        # 0 - parent child position will be randomized between mitosis event
+        # negative integer - parent appears on the 'left' of the child
+        # positive integer - parent appears on the 'right' of the child
+        self.setParentChildPositionFlag(-1)
+
+    def step(self, mcs):
+
+        cells_to_divide = []
+        for cell in self.cell_list:
+            if cell.volume > 50:
+                cells_to_divide.append(cell)
+
+        for cell in cells_to_divide:
+            # to change mitosis mode leave one of the below lines uncommented
+            self.divide_cell_random_orientation(cell)
+            # self.divideCellOrientationVectorBased(cell,1,0,0)                 # this is a valid option
+            # self.divideCellAlongMajorAxis(cell)                               # this is a valid option
+            # self.divideCellAlongMinorAxis(cell)                               # this is a valid option
+
+    def updateAttributes(self):
+    # def update_attributes(self):
+        # reducing parent target volume BEFORE cloning
+        self.parentCell.targetVolume /= 2.0
+
+        self.clone_parent_2_child()
+
+        # implementing
+        if self.parentCell.type == self.CONDENSING:
+            self.childCell.type = self.NONCONDENSING
+        else:
+            self.childCell.type = self.CONDENSING
