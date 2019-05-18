485fc95e3363c094e6de25ea803f4efb26c8e8e9
Maciek Swat <maciekswat@gmail.com>
2019-05-17 21:56:56 -0600

    added wet foam demo

--- CompuCell3D/core/Demos/Models/wetFoam/Simulation/wetFoam.py
+++ CompuCell3D/core/Demos/Models/wetFoam/Simulation/wetFoam.py
@@ -21,9 +21,9 @@ steppableRegistry=SteppableRegistry()
 
 from wetFoamSteppables import FlexCellInitializer
 fci=FlexCellInitializer(_simulator=sim,_frequency=1)
-fci.addCellTypeParameters(_type=1,_count=80,_targetVolume=25,_lambdaVolume=10.0)
-fci.addCellTypeParameters(_type=2,_count=0,_targetVolume=5,_lambdaVolume=2.0)
-fci.setFractionOfWater(0.25)
+fci.add_cell_type_parameters(cell_type=1, count=80, target_volume=25, lambda_volume=10.0)
+fci.add_cell_type_parameters(cell_type=2, count=0, target_volume=5, lambda_volume=2.0)
+fci.set_fraction_of_water(0.25)
 steppableRegistry.registerSteppable(fci)
 
 CompuCellSetup.mainLoop(sim,simthread,steppableRegistry)
