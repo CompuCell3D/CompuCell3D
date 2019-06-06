3df098176d2f2471d282aa8280f055b3d7c04f68
compucell3d <maciekswat@gmail.com>
2019-06-05 22:30:40 -0600

    added example showing how to use numpy expressions in parameter scans

---
+++ CompuCell3D/core/Demos/ParameterScan/CellSortingWithNumpyParams/Simulation/CellSortingSteppables.py
@@ -0,0 +1,25 @@
+from cc3d.core.PySteppables import *
+
+
+MYVAR={{MYVAR}}
+MYVAR1={{MYVAR1}}
+
+class CellSortingSteppable(SteppableBasePy):    
+
+    def __init__(self,frequency=1):
+        SteppableBasePy.__init__(self,frequency)
+        
+    def step(self,mcs):        
+        #type here the code that will run every _frequency MCS        
+        global MYVAR
+        
+        print ('MYVAR=',MYVAR)
+        for cell in self.cell_list:
+            if cell.type==self.DARK:
+                # Make sure ExternalPotential plugin is loaded
+                cell.lambdaVecX=-0.5 # force component pointing along X axis - towards positive X's
+                
+                
+                
+                
+        
