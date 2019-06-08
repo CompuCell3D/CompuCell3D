3f846ff887b1221c7288f57c3d4f0895e3cb7e2a
compucell3d <maciekswat@gmail.com>
2019-06-07 23:36:32 -0600

    cleanup + refactoring

--- cc3d/twedit5/twedit/utils/global_imports.py
+++ cc3d/twedit5/twedit/utils/global_imports.py
@@ -1,1 +1,1 @@
-from PyQt5.QtCore import *from PyQt5.QtGui import *from PyQt5.QtWidgets import *from PyQt5.QtNetwork import *from PyQt5.Qsci import *from PyQt5 import *import osimport sysimport time# from os.path import *
+from PyQt5.QtCore import *from PyQt5.QtGui import *from PyQt5.QtWidgets import *from PyQt5.QtNetwork import *from PyQt5.Qsci import *from PyQt5 import *from os.path import *import osimport sysimport time# from os.path import *
