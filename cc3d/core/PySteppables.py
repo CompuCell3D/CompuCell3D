9098f838b7ca3383eeb71f8ab2a7eb5870fcfb90
CompuCell3D <maciekswat@gmail.com>
2019-08-24 10:51:06 -0600

    added Hill and Michaelis-Menten functions - fixes https://github.com/CompuCell3D/CompuCell3D/issues/133

--- cc3d/core/PySteppables.py
+++ cc3d/core/PySteppables.py
@@ -18,6 +18,7 @@ from deprecated import deprecated
 from cc3d.core.SteeringParam import SteeringParam
 from copy import deepcopy
 from math import sqrt
+from cc3d.core.numerics import *
 
 
 class SteppablePy:
