2b333890f98465be37941e693e92174ecb5b4c1d
CompuCell3D <maciekswat@gmail.com>
2019-12-22 19:51:51 -0700

    Added multiprocessing capabilities for calling cc3d simulations. Users can specify number of workers and a different number of simulations and the code will process them in parallel. The results of each simulation can be returned with identifier tag meking it possible to identify which simulation run in parallel produced which result

---
+++ cc3d/cc3d_call_demo_multiprocessing.py
@@ -0,0 +1,61 @@
+import multiprocessing
+from cc3d.CC3DCaller import CC3DCaller
+from cc3d.CC3DCaller import CC3DCallerWorker
+
+
+def main():
+    num_workers = 1
+    num_jobs = 10
+
+    sim_fnames = [r'd:\CC3DProjects\demo_py3\cellsort_2D\cellsort_2D.cc3d'] * num_jobs
+
+    tasks = multiprocessing.JoinableQueue()
+    results = multiprocessing.Queue()
+
+    # Start workers
+    num_consumers = multiprocessing.cpu_count() * 2
+    print('Creating %d consumers' % num_consumers)
+    workers = [CC3DCallerWorker(tasks, results) for i in range(num_workers)]
+    for w in workers:
+        w.start()
+
+    # Enqueue jobs
+
+    for i, sim_fname in enumerate(sim_fnames):
+        cc3d_caller = CC3DCaller(cc3d_sim_fname=sim_fname,
+                                 screenshot_output_frequency=10,
+                                 output_dir=r'c:\Users\m\CC3DWorkspace\cellsort_' + f'{i}',
+                                 result_identifier_tag=i
+                                 )
+        tasks.put(cc3d_caller)
+
+    # Add a stop task "poison pill" for each of the workers
+    for i in range(num_workers):
+        tasks.put(None)
+
+    # Wait for all of the tasks to finish
+    tasks.join()
+
+    # Start printing results
+    while num_jobs:
+        result = results.get()
+        print('Result:', result)
+        num_jobs -= 1
+
+
+if __name__ == '__main__':
+    main()
+
+# ret_values = []
+# for i, sim_fname in enumerate(sim_fnames):
+#
+#     cc3d_caller = CC3DCaller()
+#
+#     cc3d_caller.cc3d_sim_fname = sim_fname
+#     cc3d_caller.screenshot_output_frequency = 10
+#     cc3d_caller.output_dir = r'c:\Users\m\CC3DWorkspace\cellsort_'+f'{i}'
+#
+#     ret_value = cc3d_caller.run()
+#     ret_values.append(ret_value)
+#
+# print ('return values', ret_values)
