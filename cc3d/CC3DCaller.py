6edad4b1aaa14934c65c0fe83ea1971a80315360
CompuCell3D <maciekswat@gmail.com>
2019-12-22 19:55:02 -0700

    cleanup

--- cc3d/CC3DCaller.py
+++ cc3d/CC3DCaller.py
@@ -1,15 +1,8 @@
-import traceback
 import cc3d
-import sys
-from os.path import *
-
-# import cc3d.CompuCellSetup as CompuCellSetup
 from cc3d import CompuCellSetup
 from cc3d.CompuCellSetup.sim_runner import run_cc3d_project
 from cc3d.core.RollbackImporter import RollbackImporter
-
 import multiprocessing
-import time
 
 
 class CC3DCaller:
@@ -30,7 +23,7 @@ class CC3DCaller:
         self.restart_multiple_snapshots = restart_multiple_snapshots
         self.output_dir = output_dir
         self.output_file_core_name = output_file_core_name
-        self.result_identifier_tag =  result_identifier_tag
+        self.result_identifier_tag = result_identifier_tag
 
     def run(self):
         persistent_globals = cc3d.CompuCellSetup.persistent_globals
@@ -57,8 +50,6 @@ class CC3DCaller:
         else:
             return {'result': persistent_globals.return_object}
 
-        # return persistent_globals.return_object
-
 
 class CC3DCallerWorker(multiprocessing.Process):
 
@@ -81,35 +72,3 @@ class CC3DCallerWorker(multiprocessing.Process):
             self.task_queue.task_done()
             self.result_queue.put(answer)
         return
-
-
-
-# class CC3DCaller:
-#     def __init__(self):
-#         self.cc3d_sim_fname = None
-#         self.output_frequency = 0
-#         self.screenshot_output_frequency = 0
-#         self.restart_snapshot_frequency = 0
-#         self.restart_multiple_snapshots = False
-#         self.output_dir = None
-#         self.output_file_core_name = None
-#
-#     def run(self):
-#
-#         persistent_globals = cc3d.CompuCellSetup.persistent_globals
-#
-#         rollback_importer = RollbackImporter()
-#
-#         persistent_globals.simulation_file_name = self.cc3d_sim_fname
-#         persistent_globals.output_frequency = self.output_frequency
-#         persistent_globals.screenshot_output_frequency = self.screenshot_output_frequency
-#         persistent_globals.set_output_dir(self.output_dir)
-#         persistent_globals.output_file_core_name = self.output_file_core_name
-#         persistent_globals.restart_snapshot_frequency = self.restart_snapshot_frequency
-#         persistent_globals.restart_multiple_snapshots = self.restart_multiple_snapshots
-#
-#         run_cc3d_project(cc3d_sim_fname=self.cc3d_sim_fname)
-#
-#         rollback_importer.uninstall()
-#
-#         return persistent_globals.return_object
--- cc3d/cc3d_call_demo_multiprocessing.py
+++ cc3d/cc3d_call_demo_multiprocessing.py
@@ -4,7 +4,7 @@ from cc3d.CC3DCaller import CC3DCallerWorker
 
 
 def main():
-    num_workers = 1
+    num_workers = 4
     num_jobs = 10
 
     sim_fnames = [r'd:\CC3DProjects\demo_py3\cellsort_2D\cellsort_2D.cc3d'] * num_jobs
@@ -46,16 +46,3 @@ def main():
 if __name__ == '__main__':
     main()
 
-# ret_values = []
-# for i, sim_fname in enumerate(sim_fnames):
-#
-#     cc3d_caller = CC3DCaller()
-#
-#     cc3d_caller.cc3d_sim_fname = sim_fname
-#     cc3d_caller.screenshot_output_frequency = 10
-#     cc3d_caller.output_dir = r'c:\Users\m\CC3DWorkspace\cellsort_'+f'{i}'
-#
-#     ret_value = cc3d_caller.run()
-#     ret_values.append(ret_value)
-#
-# print ('return values', ret_values)
