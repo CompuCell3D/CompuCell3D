05ccaef4d35503e2845ed8ef9e0f40d9fe5ed812
CompuCell3D <maciekswat@gmail.com>
2019-12-23 20:08:24 -0700

    added convenience scripts to set environment paths. Added example of simulation that returns a value

---
+++ CompuCell3D/core/Demos/CallableCC3D/environment_var_setters/cc3d_caller_env_var_set_windows.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+@SET PREFIX_CC3D=<path to where cc3d is installed>
+@SET PYTHON_INSTALL_PATH=<path to where python used for cc3d is installed>
+@SET PYTHONPATH=%PREFIX_CC3D%\lib\site-packages
