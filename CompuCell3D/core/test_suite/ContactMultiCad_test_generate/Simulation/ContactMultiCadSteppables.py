53f2b220b3313eb4747f27e75d48ea8c3ff64078
CompuCell3D <maciekswat@gmail.com>
2020-12-12 12:44:56 -0700

    added ContactMultiCad.cc3d test

---
+++ CompuCell3D/core/test_suite/ContactMultiCad_test_generate/Simulation/ContactMultiCadSteppables.py
@@ -0,0 +1,28 @@
+from cc3d.core.PySteppables import *
+import random
+
+
+class ContactMultiCadSteppable(SteppableBasePy):
+    def __init__(self, frequency=10):
+        SteppableBasePy.__init__(self, frequency)
+        self.table = None
+
+    def set_type_contact_energy_table(self, _table):
+        self.table = _table
+
+    def start(self):
+        random.seed(10)
+        c_multi_cad_data_accessor = self.contactMultiCadPlugin.getContactMultiCadDataAccessorPtr()
+
+        for cell in self.cell_list:
+
+            c_multi_cad_data = c_multi_cad_data_accessor.get(cell.extraAttribPtr)
+            # jVec.set(0,self.table[cell.type])
+            specificity_obj = self.table[cell.type]
+            print("cell.type=", cell.type, " specificity_obj=", specificity_obj)
+            if isinstance(specificity_obj, list):
+                c_multi_cad_data.assignValue(0, (specificity_obj[1] - specificity_obj[0]) * random.random())
+                c_multi_cad_data.assignValue(1, (specificity_obj[1] - specificity_obj[0]) * random.random())
+            else:
+                c_multi_cad_data.assignValue(0, specificity_obj)
+                c_multi_cad_data.assignValue(1, specificity_obj / 2)
