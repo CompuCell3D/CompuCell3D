275c16626e395406dce08f54bc07f3eaccbc3930
tjsego <tjsego@gmail.com>
2021-05-26 14:02:31 -0400

    Cleanup in ContactLocalProductPlugin

--- CompuCell3D/core/CompuCell3D/plugins/ContactLocalProduct/ContactLocalProductPlugin.h
+++ CompuCell3D/core/CompuCell3D/plugins/ContactLocalProduct/ContactLocalProductPlugin.h
@@ -28,13 +28,6 @@
 
 #include "ContactLocalProductData.h"
 
-// // // #include <CompuCell3D/Potts3D/EnergyFunction.h>
-// // // #include <BasicUtils/BasicClassAccessor.h>
-// // // #include <BasicUtils/BasicClassGroup.h> //had to include it to avoid problems with template instantiation
-
-// // // #include <CompuCell3D/Potts3D/CellGChangeWatcher.h>
-// // // #include <CompuCell3D/Plugin.h>
-// // // #include <muParser/muParser.h>
 #include "ContactLocalProductDLLSpecifier.h"
 
 class CC3DXMLElement;
@@ -51,25 +44,25 @@ namespace CompuCell3D {
 
   class CONTACTLOCALPRODUCT_EXPORT ContactLocalProductPlugin : public Plugin,public EnergyFunction {
 
-   public:
-      typedef double (ContactLocalProductPlugin::*contactEnergyPtr_t)(const CellG *cell1, const CellG *cell2);
+  public:
+    typedef double (ContactLocalProductPlugin::*contactEnergyPtr_t)(const CellG *cell1, const CellG *cell2);
 
 
-   private:
-   	ParallelUtilsOpenMP *pUtils;    
+  private:
+    ParallelUtilsOpenMP *pUtils;    
     
     BasicClassAccessor<ContactLocalProductData> contactProductDataAccessor;
     Potts3D *potts;
     Simulator *sim;
     CC3DXMLElement *xmlData;
 
-	 //Energy function data
+    //Energy function data
     typedef std::unordered_map<unsigned char, std::unordered_map<unsigned char, double> > contactSpecificityArray_t;
 
     contactSpecificityArray_t contactSpecificityArray;
     
     std::string autoName;
-	 std::string contactFunctionType;
+    std::string contactFunctionType;
     double depth;
 
     BasicClassAccessor<ContactLocalProductData> * contactProductDataAccessorPtr;
@@ -82,12 +75,12 @@ namespace CompuCell3D {
     unsigned int maxNeighborIndex;
     BoundaryStrategy *boundaryStrategy;
     float energyOffset;
-	
+
     
-	 // std::string customExpression; //expression for cad-cad function
-	 // double k1; //used to keep arguments for cad-cad function 
-	 // double k2;//used to keep arguments for cad-cad function
-	 // mu::Parser p;
+    // std::string customExpression; //expression for cad-cad function
+    // double k1; //used to keep arguments for cad-cad function 
+    // double k2;//used to keep arguments for cad-cad function
+    // mu::Parser p;
 
     //vectorized variables for convenient parallel access
     std::vector<std::string> variableNameVector;
@@ -111,15 +104,15 @@ namespace CompuCell3D {
 
 		virtual void extraInit(Simulator *simulator);
         
-        virtual void handleEvent(CC3DEvent & _event);
+    virtual void handleEvent(CC3DEvent & _event);
 
 
 
     //Steerrable interface
-	 virtual void update(CC3DXMLElement *_xmlData, bool _fullInitFlag=false);
-	 virtual std::string steerableName();
+    virtual void update(CC3DXMLElement *_xmlData, bool _fullInitFlag=false);
+    virtual std::string steerableName();
 
-	 virtual std::string toString();
+    virtual std::string toString();
 
     void setJVecValue(CellG * _cell, unsigned int _index,float _value);
     float getJVecValue(CellG * _cell, unsigned int _index);
@@ -127,23 +120,23 @@ namespace CompuCell3D {
     void setCadherinConcentration(CellG * _cell, unsigned int _index,float _value);
     float getCadherinConcentration(CellG * _cell, unsigned int _index);
 
-	//user in serialization and restart
-	void setCadherinConcentrationVec(CellG * _cell, std::vector<float> &_vec);
-	std::vector<float> getCadherinConcentrationVec(CellG * _cell);
+    //user in serialization and restart
+    void setCadherinConcentrationVec(CellG * _cell, std::vector<float> &_vec);
+    std::vector<float> getCadherinConcentrationVec(CellG * _cell);
 
-	//EnergyFunction fcns
-	 double contactSpecificity(const CellG *cell1, const CellG *cell2);
+    //EnergyFunction fcns
+    double contactSpecificity(const CellG *cell1, const CellG *cell2);
 
 
     double contactEnergyLinear(const CellG *cell1, const CellG *cell2);
     double contactEnergyQuadratic(const CellG *cell1, const CellG *cell2);
     double contactEnergyMin(const CellG *cell1, const CellG *cell2);
-	 double contactEnergyCustom(const CellG *cell1, const CellG *cell2);
+	  double contactEnergyCustom(const CellG *cell1, const CellG *cell2);
 
     double contactEnergyLinearMediumLocal(const CellG *cell1, const CellG *cell2);
     double contactEnergyQuadraticMediumLocal(const CellG *cell1, const CellG *cell2);
     double contactEnergyMinMediumLocal(const CellG *cell1, const CellG *cell2);
-	double contactEnergyCustomMediumLocal(const CellG *cell1, const CellG *cell2);
+	  double contactEnergyCustomMediumLocal(const CellG *cell1, const CellG *cell2);
 
 
     /**
