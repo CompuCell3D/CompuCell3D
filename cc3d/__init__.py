c78dfb6010a1fcfd4e2b40b5aef6bec20291c950
compucell3d <compucell3dproject@gmail.com>
2022-04-13 19:23:21 -0700

    updated version in __init__.py

--- cc3d/__init__.py
+++ cc3d/__init__.py
@@ -10,8 +10,8 @@ from os.path import dirname, join, abspath
 from pathlib import Path
 
 __version__ = "4.3.0"
-__revision__ = "1"
-__githash__ = "262904e6"
+__revision__ = "2"
+__githash__ = "a690adaa"
 
 from . import config
 
