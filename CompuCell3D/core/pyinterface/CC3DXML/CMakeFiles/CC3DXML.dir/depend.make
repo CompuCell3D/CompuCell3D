3971dfc58db34b8dbee4cdaf72fa0d8134ae4627
DeconvFFT <mehta.saumya29@gmail.com>
2022-06-13 15:16:05 -0400

    removed cmake related files using gitignore

--- CompuCell3D/core/pyinterface/CC3DXML/CMakeFiles/CC3DXML.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for CC3DXML.
-# This may be replaced when dependencies are built.
--- CompuCell3D/core/pyinterface/SerializerDE/CMakeFiles/cmake_directory_clean.cmake
+++
@@ -1,5 +0,0 @@
-file(REMOVE_RECURSE
-  "core/pyinterface/SerializerDE/CMakeFiles/SerializerDEPy.dir/SerializerDEPyPYTHON_wrap.cxx"
-  "core/pyinterface/SerializerDE/SerializerDEPy.py"
-  "core/pyinterface/SerializerDE/CMakeFiles/SerializerDEPy.dir/SerializerDEPyPYTHON.stamp"
-)
