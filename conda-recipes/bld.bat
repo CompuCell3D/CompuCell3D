e503100264aefdadeb78bad01ffae8b0f5f76c12
tjsego <tjsego@gmail.com>
2022-07-08 09:32:25 -0400

    Upgraded msvc to 2019

--- conda-recipes/bld.bat
+++ conda-recipes/bld.bat
@@ -4,6 +4,7 @@ mkdir build
 cd build
 
 cmake -DCMAKE_BUILD_TYPE:STRING="%BUILD_CONFIG%" ^
+      -G "Ninja" ^
       -DCMAKE_PREFIX_PATH="%PREFIX%" ^
       -DCMAKE_FIND_ROOT_PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
