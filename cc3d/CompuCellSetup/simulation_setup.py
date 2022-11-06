013ea92e803a7aa3693f170728cbcb46976b052c
compucell3d <maciekswat@gmail.com>
2022-11-05 19:14:31 -0600

    added commented out line that turns off console debug printouts

--- cc3d/CompuCellSetup/simulation_setup.py
+++ cc3d/CompuCellSetup/simulation_setup.py
@@ -21,6 +21,7 @@ from cc3d.core.Validation.sanity_checkers import validate_cc3d_entity_identifier
 from cc3d.CompuCellSetup.cluster_utils import check_nanohub_and_count
 
 CompuCell.Logger.enableConsoleLogging(CompuCell.LOG_DEBUG)
+# CompuCell.Logger.disableConsoleLogging(CompuCell.LOG_DEBUG)
 
 # -------------------- legacy API emulation ----------------------------------------
 def getCoreSimulationObjects():
