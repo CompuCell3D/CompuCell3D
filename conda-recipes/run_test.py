ca0395ec0ee8a584f4be8def751f19a67d726f84
maciekswat <maciekswat@gmail.com>
2023-02-12 12:12:44 -0700

    added plugin regression testing to conda build

--- conda-recipes/run_test.py
+++ conda-recipes/run_test.py
@@ -1,11 +1,17 @@
 from cc3d.tests import simulation_tester
+from cc3d.tests.plugin_test_suite import regression_tests_runner
 from cc3d import cc3d_scripts_path
 import os
 import subprocess
 import sys
+from pathlib import Path
+
+test_plugin_dir = Path.cwd().joinpath('tests_regression')
+test_plugin_dir.mkdir(exist_ok=True, parents=True)
 
 
 test_dir = os.path.join(os.getcwd(), 'tests')
+
 if not os.path.isdir(test_dir):
     os.mkdir(test_dir)
 
@@ -31,7 +37,7 @@ if not os.path.isfile(run_script):
 # test_regression_errors.csv - reports actual regression testing errors
 fail_file = os.path.join(test_dir, 'test_regression_errors.csv')
 
-
+plugin_test_suite_fail_file = test_plugin_dir.joinpath('regression_test_errors.csv')
 
 
 def main():
@@ -42,11 +48,21 @@ def main():
 
     # from cc3d.cpp import CompuCell
     # sys.exit(0)
+    # run pde solvers regression suite
     subprocess.check_call(args=['python', simulation_tester.__file__,
                                 f'--run-command={run_script}',
                                 f'--output-dir={test_dir}'],
                           cwd=exec_dir)
     if os.path.isfile(fail_file):
+        sys.exit(1)
+
+    # run plugin regression suite
+    subprocess.check_call(args=['python', regression_tests_runner.__file__,
+                                f'--run-command={run_script}',
+                                f'--output-dir={test_plugin_dir}'],
+                          cwd=exec_dir)
+
+    if plugin_test_suite_fail_file.exists():
         sys.exit(1)
 
 
