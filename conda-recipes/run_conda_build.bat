401c0cd1c0073637466e57b81dfb923f85bb67be
maciekswat <maciekswat@gmail.com>
2023-02-26 16:44:22 -0700

    switched to using versions.yaml for conda builds - using environment variables did not work on windows...

--- conda-recipes/run_conda_build.bat
+++ conda-recipes/run_conda_build.bat
@@ -1,12 +1,2 @@
-set version="4.4.0"
-set build_number=0
-set numpy_version="1.21"
-set cc3d_network_solvers_version="0.3.0"
-set cmake_version="3.21" 
-set vtk_version="9.2" 
-set swig_version="4" 
-set boost_version="1.78" 
-set tbb_devel_version="2021" 
-
 conda build -c conda-forge -c compucell3d . --python=3.7
 #conda render .
