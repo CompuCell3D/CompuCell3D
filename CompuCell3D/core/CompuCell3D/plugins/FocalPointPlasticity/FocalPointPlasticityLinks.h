613b3796a84788e707c994b6a092c0b4962ad3b0
maciekswat <maciekswat@gmail.com>
2023-10-07 19:00:42 -0600

    Fixing issue with attaching Python dictionary to Link objects. For some reason #ifdef SWIGPYTHON macro did not work with swig 4.0. I moved dictionary creation to CompuCellExtraDeclarations.i.

--- CompuCell3D/core/CompuCell3D/plugins/FocalPointPlasticity/FocalPointPlasticityLinks.h
+++ CompuCell3D/core/CompuCell3D/plugins/FocalPointPlasticity/FocalPointPlasticityLinks.h
@@ -193,9 +193,14 @@ namespace CompuCell3D {
         PyObject *pyAttrib;
 
         PyObject *getPyAttrib() {
-#ifdef SWIGPYTHON
-            if (pyAttrib == nullptr) pyAttrib = PyDict_New();
-#endif
+//#ifdef SWIG
+//            if (pyAttrib == nullptr){
+//                PyGILState_STATE gstate;
+//                gstate = PyGILState_Ensure();
+//                pyAttrib = PyDict_New();
+//                PyGILState_Release(gstate);
+//            }
+//#endif
             return pyAttrib;
         }
     };
