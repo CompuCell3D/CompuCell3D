6cf261b8e4f1c5780af5911c45e2d7a0a357f973
compucell3d <maciekswat@gmail.com>
2023-10-28 17:23:38 -0600

    fixing OSX compilation issue on OSX

--- CompuCell3D/core/CompuCell3D/steppables/TubeFieldInitializer/TubeFieldInitializer.cpp
+++ CompuCell3D/core/CompuCell3D/steppables/TubeFieldInitializer/TubeFieldInitializer.cpp
@@ -304,7 +304,7 @@ void TubeFieldInitializer::layOutCells(const TubeFieldInitializerData &_initData
     //TODO does it work with extra-large tubes?
 
     //Do a linear interpolation between fromPoint and toPoint
-    short numAxisPoints = tubeLength / max(short(_initData.gap), 1) + 1;
+    short numAxisPoints = tubeLength / max(int(_initData.gap), 1) + 1;
     // if (_initData.gap != 0)
     //     numAxisPoints /= 
     // numAxisPoints 
