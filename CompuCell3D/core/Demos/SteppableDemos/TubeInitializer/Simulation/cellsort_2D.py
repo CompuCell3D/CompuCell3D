ed9d1fb71397d925f2bca91ed7d9772e95f007ad
compucell3d <maciekswat@gmail.com>
2023-11-18 16:10:17 -0700

    addressed PR comments https://github.com/CompuCell3D/CompuCell3D/pull/683 and also renamed simulation file names

--- CompuCell3D/core/Demos/SteppableDemos/TubeInitializer/Simulation/cellsort_2D.py
+++
@@ -1,7 +0,0 @@
-from cc3d import CompuCellSetup
-
-from cellsort_2DSteppables import cellsort_2DSteppable
-
-CompuCellSetup.register_steppable(steppable=cellsort_2DSteppable(frequency=1))
-
-CompuCellSetup.run()
