712aebf73ac7f87662b262453bf492eccc701882
Pete Fyffe <fyffep>
2023-11-19 11:36:13 -0500

    Fixed MCS on movie text; centered movie text

--- cc3d/core/GraphicsUtils/MovieCreator.py
+++ cc3d/core/GraphicsUtils/MovieCreator.py
@@ -4,6 +4,7 @@ import subprocess
 import tempfile
 import shutil
 
+
 def makeMovie(simulationPath, frameRate, quality):
     """
     :param simulationPath: a string path to a directory with a .cc3d file and screenshot directories
@@ -11,7 +12,8 @@ def makeMovie(simulationPath, frameRate, quality):
     :param quality: an int 1-10 (inclusive)
     :return: the number of movies created
     """
-    #Credit to https://stackoverflow.com/q/49581846/16519580 user 'Makes' for the text overlay FFMPEG command.
+    # Credit to https://stackoverflow.com/q/49581846/16519580 user 'Makes' for the text overlay FFMPEG command.
+    # Credit to https://superuser.com/a/939386 uer 'llogan' for the text positioning in the FFMPEG command.
 
     if not os.path.exists(simulationPath):
         print(f"Error: Could not make movie inside unknown directory `{simulationPath}`")
@@ -27,18 +29,32 @@ def makeMovie(simulationPath, frameRate, quality):
 
         # 'delete=True' removes the temporary file when it is closed.
         # So, setting 'delete=True' ensures that the tempfile stays active long enough for FFMPEG to read it.
-        with tempfile.NamedTemporaryFile(delete=False, mode='+a', dir=inputPath) as temp_file:
-            with tempfile.NamedTemporaryFile(delete=False, mode='+a', dir=inputPath) as text_overlay_file:
-
+        with tempfile.NamedTemporaryFile(delete=False, mode='+a', dir=inputPath) as tempFile:
+            with tempfile.NamedTemporaryFile(delete=False, mode='+a', dir=inputPath) as textOverlayFile:
+                """
+                Write the names of files to be used as video frames to tempFile 
+                and write the text to draw to textOverlayFile.
+                """
                 frameCount = 0
-                for fileName in os.listdir(inputPath):
-                    if fileName.lower().endswith(".png"):
-                        temp_file.write(f"file '{fileName}'\n")
-                        text_overlay_file.write(f"{frameCount} drawtext reinit 'text=MCS {frameCount}';\n")
+                for fileNameExt in os.listdir(inputPath):
+                    fileName, fileExtension = os.path.splitext(fileNameExt)
+                    if fileExtension.lower() == ".png":
+                        tempFile.write(f"file '{fileNameExt}'\n")
+
+                        # Try to use the MCS listed in the screenshot name
+                        mcs = 0
+                        try:
+                            parts = fileName.split("_")
+                            if len(parts) >= 2:
+                                mcs = int(parts[-1])
+                        except:
+                            mcs = frameCount
+
+                        # Center the text
+                        textOverlayFile.write(f"{frameCount} drawtext reinit 'text=MCS {mcs}':x=(w-text_w)/2:y=0;\n")
                         frameCount += 1
-                temp_file.close()
-                text_overlay_file.close()
-                print("Wrote to text_overlay_file",text_overlay_file.name)
+                tempFile.close()
+                textOverlayFile.close()
 
                 if frameCount > 0:
                     # Number the file name so that it does not overwrite another movie
@@ -58,19 +74,19 @@ def makeMovie(simulationPath, frameRate, quality):
                         "-r", str(frameRate),  # output frame rate
                         "-f", "concat",
                         "-safe", "0",
-                        "-i", temp_file.name,
+                        "-i", tempFile.name,
                         "-crf", str(quality),  # set quality (constant rate factor, crf): 51=worst, 0=best
                         "-c:v", "libx264",  # video codec: H.264
                         "-pix_fmt", "yuv420p",
-                        "-filter_complex", f"[0:v]sendcmd=f={os.path.basename(text_overlay_file.name)},drawtext=fontfile=PF.ttf:text='':fontcolor=white:fontsize=20",
+                        "-filter_complex", f"[0:v]sendcmd=f={os.path.basename(textOverlayFile.name)},drawtext=fontfile=PF.ttf:text='':fontcolor=white:fontsize=20",
                         outputPath
                     ], cwd=inputPath)
 
                     if os.path.exists(outputPath):
                         movieCount += 1
 
-                os.remove(text_overlay_file.name)
-            os.remove(temp_file.name)
+                os.remove(textOverlayFile.name)
+            os.remove(tempFile.name)
 
     print(f"Created {movieCount} movies inside `{simulationPath}`")
     return movieCount
