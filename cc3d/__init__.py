c87049582439fbab9b89f2e1b24f25f446e9f36a
compucell3d <maciekswat@gmail.com>
2024-01-06 17:17:47 -0700

    updated version number for boost and for cc3d

--- cc3d/__init__.py
+++ cc3d/__init__.py
@@ -10,8 +10,8 @@ from os.path import dirname, join, abspath
 from pathlib import Path
 
 __version__ = "4.5.0"
-__revision__ = "0"
-__githash__ = "2daa7b7"
+__revision__ = "1"
+__githash__ = "a8a3d1f"
 
 # from . import config
 from cc3d import config
