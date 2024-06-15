b712f963537cfb00f2fb52bcb233e059293cd000
compucell3d <maciekswat@gmail.com>
2024-06-15 17:49:49 -0600

    updated revision and commit tag

--- cc3d/__init__.py
+++ cc3d/__init__.py
@@ -10,8 +10,8 @@ from os.path import dirname, join, abspath
 from pathlib import Path
 
 __version__ = "4.6.0"
-__revision__ = "0"
-__githash__ = "240bc65"
+__revision__ = "1"
+__githash__ = "a990885"
 
 
 from cc3d import config
